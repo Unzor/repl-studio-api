03e76a82b5ba0bd7329735614fa131c0f1fcbb41
Unzor <71789103+Unzor@users.noreply.github.com>
2021-07-20 18:29:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+ const _interpreter=require("jsinterpret");
 var _result;
   var express = require('express')
 var app = express()
@@ -17,186 +18,34 @@ app.use(bodyParser());
 
 app.get('/', function (req, res) {
 res.send(`APIs: <div></div>
-/interpretJS`)
+/interpretJS<div></div>
+/interpretScheme`)
 })
 
-app.post('/interpretJS', function (req, rees) {
-  console.log=function(a){rees.json({result: a})}
-            function replaceAll(string, search, replace) {
-  return string.split(search).join(replace);
-}
-
-const util = require("util");
-const repl = require("repl");
-const path = require("path");
-const fs = require("fs");
-const vm = require("vm");
-const rl = require("./readline-sync.js");
-const Module = require("module");
-
-let r;
-
-// Red errors.
-function logError(msg) {
-  process.stdout.write("\u001b[0m\u001b[31m" + msg + "\u001b[0m");
-}
-
-// The nodejs repl operates in raw mode and does some funky stuff to
-// the terminal. This ns the repl and forces non-raw mode.
-function pauseRepl() {
-  if (!r) return;
-
-  r.pause();
-  process.stdin.setRawMode(false);
-}
-
-// Forces raw mode and resumes the repl.
-function resumeRepl() {
-  if (!r) return;
-
-  process.stdin.setRawMode(true);
-  r.resume();
-}
-
-// Clear the line if it has anything on it.
-function clearLine() {
-  if (r && r.line) r.clearLine();
-}
-
-// Adapted from the internal node repl code just a lot simpler and adds
-// red errors (see https://bit.ly/2FRM86S)
-function handleError(e) {
-  if (r) {
-    r.lastError = e;
-  }
-
-  if (e && typeof e === "object" && e.stack && e.name) {
-    if (e.name === "SyntaxError") {
-      e.stack = e.stack
-        .replace(/^repl:\d+\r?\n/, "")
-        .replace(/^\s+at\s.*\n?/gm, "");
-    }
-
-    logError(e.stack);
-  } else {
-    // For some reason needs a newline to flush.
-    logError("Thrown: " + r.writer(e) + "\n");
-  }
-
-  if (r) {
-    r.clearBufferedCommand();
-    r.lines.level = [];
-    r.displayPrompt();
-  }
-}
-
-function start(context) {
-  r = repl.start({
-    prompt: process.env.Y_PS1,
-    useGlobal: true,
-  });
-
-  // remove the internal error and ours for red etc.
-  r._domain.removeListener("error", r._domain.listeners("error")[0]);
-  r._domain.on("error", handleError);
-  process.on("uncaughtException", handleError);
-}
-
-global.alert = console.log;
-global.prompt = (p) => {
-  pauseRepl();
-  clearLine();
-
-  let ret = rl.question(`${p}> `, {
-    hideEchoBack: false,
-  });
-
-  resumeRepl();
-
-  // Display prompt on the next turn.
-  if (r) setImmediate(() => r.displayPrompt());
-
-  return ret;
-};
-
-global.confirm = (q) => {
-  pauseRepl();
-  clearLine();
-
-  const ret = rl.keyInYNStrict(q);
-
-  resumeRepl();
-
-  // Display prompt on the next turn.
-  if (r) setImmediate(() => r.displayPrompt());
-  return ret;
-};
-
-if (req.body.code) {
-  vm.runInThisContext(req.body.code);
-  if (process.env.Y_INTERACTIVE) {
-    start();
-  }
-} else if (process.env.Y_EXP) {
- console.log(vm.runInThisContext(process.env.Y_EXP));
-  if (process.env.Y_INTERACTIVE) {
-    start();
-  }
-} else if (process.env.Y_FILE) {
-  const mainPath = path.resolve(process.env.Y_FILE);
-  const main = fs.readFileSync(mainPath, "utf-8");
-  const module = new Module(mainPath, null);
-
-  module.id = ".";
-  module.filename = mainPath;
-  module.paths = Module._nodeModulePaths(path.dirname(mainPath));
-
-  process.mainModule = module;
-
-  global.module = module;
-  global.require = module.require.bind(module);
-  global.__dirname = path.dirname(mainPath);
-  global.__filename = mainPath;
-
-  console.log(
-    "\u001b[0m\u001b[90mHint: hit control+c anytime to enter REPL.\u001b[0m"
-  );
-
-  let script;
-  try {
-    script = vm.createScript(req.body.code, {
-      filename: mainPath,
-      displayErrors: false,
+app.post('/interpretScheme', function (req, res) {
+    console.log=function(a){res.json({result: a})}
+var lips = require('@jcubic/lips');
+lips.exec(req.body.code).then(result => {
+    result.forEach(function(result) {
+        if (typeof result !== 'undefined') {
+console.log(result);
+        }
     });
-  } catch (e) {
-    handleError(e);
-  }
+});
+});
 
-  if (script) {
-    let res;
-    try {
-      res = script.runInThisContext({
-        displayErrors: false,
-      });
-     } catch (e) {
-      handleError(e);
-    }
+app.post('/interpretJS', function (req, res) {
+     console.log=function(a){
+   res.json({result: a});
+   };
+ _interpreter.interpret(req.body.code, function(a){
+console.log(a);
+ });
+});
 
-    module.loaded = true;
-
-    if (typeof res !== "undefined") {
-      console.log(util.inspect(res, { colors: true }));
-    }
-  }
-
-  if (process.env.Y_INTERACTIVE) {
-    process.once("SIGINT", () => start());
-    process.once("beforeExit", () => start());
-  }
-} else if (process.env.Y_INTERACTIVE) {
-  start();
-}
-
+app.post('/addPackage', function(req, res){
+const shell=require('shelljs');
+shell.exec("npm install " + req.body.package);
 });
 
 
